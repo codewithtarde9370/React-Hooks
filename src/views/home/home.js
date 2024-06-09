e1f401cc0e7b01aa445996ded1dfc2a6fad48d4a
codewithtarde9370 <teja36491@gmail.com>
2024-06-09 11:41:52 +0530

    added and chnaged files for to do app

---
+++ src/views/home/home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function home() {
+  return (
+    <>
+
+    
+      <h1>To-do-app</h1>
+      <div className='app-container'>
+        <div className='list-container'>
+          
+        </div>
+        <div className='btn-ip'>
+<input>
+</input>
+        </div>
+
+
+      </div>
+
+
+    
+    </>
+  )
+}
+
+export default home
